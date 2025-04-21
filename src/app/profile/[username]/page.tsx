d7ec496ff448985a58ec9d1014a2b6b3e7773e53
AmirAli <amiralitehrani77@yahoo.com>
2025-04-21 00:55:18 -0400

    Refactor generateMetadata function to destructure username from params promise

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -7,8 +7,13 @@ import {
 import { notFound } from "next/navigation";
 import ProfilePageClient from "./ProfilePageClient";
 
-export async function generateMetadata({ params }: { params: { username: string } }) {
-    const user = await getProfileByUsername(params.username);
+type Props = {
+    params: Promise<{ username: string }>
+  }
+
+export async function generateMetadata({ params }: Props) {
+    const { username } = await params;
+    const user = await getProfileByUsername(username);
     if (!user) return;
 
     return {
